60ce4908963d1c62a0048eb6314f4303930f3362
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-24 17:54:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,4 +7,4 @@ Nokogiri::HTML(html)
 
 doc = Nokogiri::HTML(html)
 p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
-courses = doc.css()
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
