64514ca46b087149c1029da92e9dc1ad7d758666
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-24 17:57:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,10 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
-
-Nokogiri::HTML(html)
-
 doc = Nokogiri::HTML(html)
-p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
-courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
-#3c239880-48cf-5ce7-84d0-578546d87586" 
+
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each do |course|
+  p course.text.strip 
+end
